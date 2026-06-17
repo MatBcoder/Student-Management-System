84f572256fd314a69634633281754e65da290419
Matb <matb.coder@gmail.com>
2026-06-17 12:36:07 +0200

    GlobalExceptionHandler to handle exceptions and returns JSON file

--- src/main/java/com/proj1/spring1proj/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proj1/spring1proj/exception/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.proj1.spring1proj.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleStudentNotFound(StudentNotFoundException ex){
+        Map<String, Object> error = Map.of(
+                "timeStamp", LocalDateTime.now(),
+                "Status", 404,
+                "Message", ex.getMessage()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
