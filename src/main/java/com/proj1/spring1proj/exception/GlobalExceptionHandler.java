16750c92e549c9930738347692fb5a5e63938aea
Matb <matb.coder@gmail.com>
2026-06-22 17:23:49 +0200

    Updated GlobalExceptionHandler to correctly handle the validity exception

--- src/main/java/com/proj1/spring1proj/exception/GlobalExceptionHandler.java
+++ src/main/java/com/proj1/spring1proj/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.proj1.spring1proj.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -18,5 +20,22 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
     }
 }
